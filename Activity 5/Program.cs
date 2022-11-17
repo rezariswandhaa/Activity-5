070d2b079aea525d4f21b1d0444662500d06ddc1
TA1137 <muhammad.reza.ft21@mail.umy.ac.id>
2022-11-17 18:42:50 +0700

    Add method del note

--- Activity 5/Program.cs
+++ Activity 5/Program.cs
@@ -86,7 +86,29 @@ namespace Double_Linked_List_Algorithm
             return (current != null);
         }
 
-        
+        public bool delNode(int rollNo)
+        {
+            Node previous, current;
+            previous = current = null;
+            if (Search(rollNo, ref previous, ref current) == false)
+                return false;
+            if (current == START)
+            {
+                START = START.next;
+                if (START != null)
+                    START.prev = null;
+                return true;
+            }
+            if (current.next == null)
+            {
+                previous.next = null;
+                return true;
+            }
+
+            previous.next = current.next;
+            current.next.prev = previous;
+            return true;
+        }
 
         
 
