33dd8abd71c47931a234f80cf23dff9c9ffe89f9
TA1137 <muhammad.reza.ft21@mail.umy.ac.id>
2022-11-17 16:19:06 +0700

    add DoublelLinkedList and AddNode

--- Activity 5/Program.cs
+++ Activity 5/Program.cs
@@ -8,6 +8,44 @@ namespace Activity_5
 {
     internal class Program
     {
+        class Node
+        {
+            /*Node class represent the node of doubly linked list.
+             * It consist of the information part and link to
+             * its succeeding and preceeding node
+             * int terms of next and previous nodes.*/
+            public int rollNumber;
+            public string name;
+            public Node next;/*points to the succeeding node*/
+            public Node prev;/*points to the preceeding node*/
+        }
+        class DoubleLinkedList
+        {
+            Node START;
+            public DoubleLinkedList()
+            {
+                START = null;
+            }
+            public void addNode()/* Add a new code */
+            {
+                int rollNo;
+                string nm;
+                Console.Write("\nEnter the roll number of the student: ");
+                rollNo = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\nEnter the name of student: ");
+                nm = Console.ReadLine();
+                Node newnode = new Node();
+                newnode.rollNumber = rollNo;
+                newnode.name = nm;
+                /*Checks if the list is empty*/
+                if (START == null || rollNo <= START.rollNumber)
+                {
+
+                }
+            }
+        }
+
+
         static void Main(string[] args)
         {
         }
